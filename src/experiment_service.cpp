b068d3e32195d4b5630d2a554e89bbe4dd24feea
German Espinosa <germanespinosa@gmail.com>
2022-11-05 18:18:18 -0500

    new homeography, spawn loc working

--- src/experiment_service.cpp
+++ src/experiment_service.cpp
@@ -1,5 +1,6 @@
 #include <experiment/experiment_service.h>
 #include <filesystem>
+#include <performance.h>
 
 using namespace tcp_messages;
 using namespace cell_world;
@@ -17,42 +18,64 @@ namespace experiment {
         return logs_path + get_experiment_prefix(experiment_name) + '/' + experiment_name + "/" + experiment_name + "_experiment.json";
     }
 
+    string get_episode_folder(const string &experiment_name, unsigned int episode_number){
+        std::stringstream ss;
+        ss << "episode_" << std::setw(3) << std::setfill('0') << episode_number;
+        auto episode = ss.str();
+        return logs_path + get_experiment_prefix(experiment_name) + '/' + experiment_name + "/" + episode;
+    }
+
+    string get_episode_file(const string &experiment_name, unsigned int episode_number){
+        std::stringstream ss;
+        ss << "episode_" << std::setw(3) << std::setfill('0') << episode_number;
+        auto episode = ss.str();
+        return get_episode_folder(experiment_name, episode_number) +  "/" + experiment_name + "_" + episode + ".json";
+    }
+
     Start_experiment_response Experiment_service::start_experiment(const Start_experiment_request &parameters) {
+        PERF_SCOPE("Experiment_service::start_experiment");
         auto server = (Experiment_server *)_server;
         return server->start_experiment(parameters);
     }
 
     bool Experiment_service::start_episode(const Start_episode_request &parameters) {
+        PERF_SCOPE("Experiment_service::start_episode");
         auto server = (Experiment_server *)_server;
         return server->start_episode(parameters);
     }
 
     bool Experiment_service::finish_episode() {
+        PERF_SCOPE("Experiment_service::finish_episode");
         auto server = (Experiment_server *)_server;
         return server->finish_episode();
     }
 
     bool Experiment_service::finish_experiment(const Finish_experiment_request &parameters) {
+        PERF_SCOPE("Experiment_service::finish_experiment");
         auto server = (Experiment_server *)_server;
         return server->finish_experiment(parameters);
     }
 
     bool Experiment_service::capture(const Capture_request &parameters) {
+        PERF_SCOPE("Experiment_service::capture");
         auto server = (Experiment_server *)_server;
         return server->capture(parameters);
     }
 
     bool Experiment_service::set_behavior(const Set_behavior_request &request) {
+        PERF_SCOPE("Experiment_service::set_behavior");
         auto server = (Experiment_server *)_server;
         return server->set_behavior(request);
     }
 
     bool Experiment_service::prey_enter_arena() {
+        PERF_SCOPE("Experiment_service::prey_enter_arena");
         auto server = (Experiment_server *)_server;
         return server->prey_enter_arena();
     }
 
     Get_experiment_response Experiment_service::get_experiment(const Get_experiment_request &parameters) {
+        PERF_SCOPE("Experiment_service::get_experiment");
         Get_experiment_response response;
         if (!cell_world::file_exists(get_experiment_file(parameters.experiment_name))) return response;
         auto experiment = json_cpp::Json_from_file<Experiment>(get_experiment_file(parameters.experiment_name));
@@ -65,7 +88,7 @@ namespace experiment {
         response.experiment_name = experiment.name;
         response.start_date = experiment.start_time;
         response.duration = experiment.duration;
-        response.episode_count = experiment.episodes.size();
+        response.episode_count = experiment.episode_count;
         response.remaining_time = remaining;
         response.subject_name = experiment.subject_name;
         return response;
@@ -88,6 +111,7 @@ namespace experiment {
     }
 
     void Experiment_tracking_client::on_step(const Step &step) {
+        PERF_SCOPE("Experiment_tracking_client::on_step");
         if (experiment_server->episode_in_progress){
             experiment_server->step_insertion_mtx.lock();
             experiment_server->active_episode.trajectories.push_back(step);
@@ -96,25 +120,32 @@ namespace experiment {
     }
 
     bool Experiment_server::start_episode(const Start_episode_request &parameters) {
+        PERF_SCOPE("Experiment_server::start_episode");
         if (episode_in_progress) return false;
         if (cell_world::file_exists(get_experiment_file(parameters.experiment_name))){
             active_experiment = parameters.experiment_name;
             active_episode = Episode();
             //active_episode.trajectories.reserve(50000);
             episode_in_progress = true;
-            if (!clients.empty()) broadcast_subscribed(tcp_messages::Message("episode_started",active_experiment));
-            for (auto &local_client:subscribed_local_clients) local_client->on_episode_started(active_experiment);
+            thread( [this]() {
+                if (!clients.empty()) broadcast_subscribed(tcp_messages::Message("episode_started", active_experiment));
+                for (auto &local_client: subscribed_local_clients) local_client->on_episode_started(active_experiment);
+            }).detach();
             return true;
         }
         return false;
     }
 
     bool Experiment_server::finish_episode() {
+        PERF_SCOPE("Experiment_server::finish_episode");
         if (!episode_in_progress) return false;
         if (!cell_world::file_exists(get_experiment_file(active_experiment))) return false;
         auto experiment = json_cpp::Json_from_file<Experiment>(get_experiment_file(active_experiment));
         active_episode.end_time = json_cpp::Json_date::now();
-        experiment.episodes.push_back(active_episode);
+        //experiment.episodes.push_back(active_episode);
+        create_folder(get_episode_folder(active_experiment, experiment.episode_count));
+        active_episode.save(get_episode_file(active_experiment, experiment.episode_count));
+        experiment.episode_count++;
         experiment.save(get_experiment_file(active_experiment));
         episode_in_progress = false;
         if (!clients.empty()) broadcast_subscribed(tcp_messages::Message("episode_finished",active_experiment));
@@ -123,6 +154,7 @@ namespace experiment {
     }
 
     bool Experiment_server::finish_experiment(const Finish_experiment_request &parameters) {
+        PERF_SCOPE("Experiment_server::finish_experiment");
         if (!cell_world::file_exists(get_experiment_file(active_experiment))) return false;
         auto experiment = json_cpp::Json_from_file<Experiment>(get_experiment_file(parameters.experiment_name));
         auto end_time = experiment.start_time + chrono::minutes(experiment.duration);
@@ -137,6 +169,7 @@ namespace experiment {
     }
 
     bool Experiment_server::capture(const Capture_request &request) {
+        PERF_SCOPE("Experiment_server::capture");
         if (episode_in_progress) {
             active_episode.captures.push_back(request.frame);
             if (!clients.empty()) broadcast_subscribed(Message("capture",request.frame));
@@ -147,12 +180,14 @@ namespace experiment {
     }
 
     bool Experiment_server::set_behavior(const Set_behavior_request &request) {
+        PERF_SCOPE("Experiment_server::set_behavior");
         if (!clients.empty()) broadcast_subscribed(Message("behavior_set", request.behavior));
         for (auto &local_client:subscribed_local_clients) local_client->on_behavior_set(request.behavior);
         return true;
     }
 
     bool Experiment_server::prey_enter_arena() {
+        PERF_SCOPE("Experiment_server::prey_enter_arena");
         if (!clients.empty()) broadcast_subscribed(Message("prey_entered_arena"));
         for (auto &local_client:subscribed_local_clients) local_client->on_prey_entered_arena();
         return true;
@@ -166,6 +201,7 @@ namespace experiment {
     }
 
     Start_experiment_response Experiment_server::start_experiment(const Start_experiment_request &parameters) {
+        PERF_SCOPE("Experiment_server::start_experiment");
         std::filesystem::create_directories(logs_path + '/' + parameters.prefix);
         Experiment new_experiment;
         new_experiment.world_configuration_name = parameters.world.world_configuration;
@@ -189,30 +225,42 @@ namespace experiment {
     }
 
     bool Experiment_service::human_intervention(const Human_intervention_request &request) {
+        PERF_SCOPE("Experiment_service::human_intervention");
         auto server = (Experiment_server *)_server;
         return server->prey_enter_arena();
     }
 
     bool Experiment_server::human_intervention(const Human_intervention_request &request) {
+        PERF_SCOPE("Experiment_server::human_intervention");
         if (!clients.empty()) broadcast_subscribed(tcp_messages::Message("human_intervention",request));
         for (auto &local_client:subscribed_local_clients) local_client->on_human_intervention(request.active);
         return false;
     }
 
     Resume_experiment_response Experiment_service::resume_experiment(const Resume_experiment_request &parameters) {
+        PERF_SCOPE("Experiment_service::resume_experiment");
         auto server = (Experiment_server *)_server;
         return server->resume_experiment(parameters);
     }
 
     Resume_experiment_response Experiment_server::resume_experiment(const Resume_experiment_request &parameters) {
+        PERF_SCOPE("Experiment_server::resume_experiment");
         Resume_experiment_response r;
         if (cell_world::file_exists(get_experiment_file(parameters.experiment_name))){
             auto experiment = json_cpp::Json_from_file<Experiment>(get_experiment_file(active_experiment));
             // state the interruption as an empty episode
+            auto interruption_start_time = experiment.start_time;
+            if  (experiment.episode_count > 0){
+                auto last_episode =  json_cpp::Json_from_file<Episode>(get_episode_file(active_experiment, experiment.episode_count - 1));
+                interruption_start_time = last_episode.end_time;
+            }
             active_episode = Episode();
-            active_episode.start_time = experiment.episodes.back().end_time;
+            active_episode.start_time = interruption_start_time;
             active_episode.end_time = json_cpp::Json_date::now();
-            experiment.episodes.push_back(active_episode);
+            create_folder(get_episode_folder(active_experiment, experiment.episode_count));
+            active_episode.save(get_episode_file(active_experiment, experiment.episode_count));
+            experiment.episode_count ++;
+            //experiment.episodes.push_back(active_episode);
             //
             experiment.duration += parameters.duration_extension;
             experiment.save(get_experiment_file(active_experiment)); // saves the interruption and the extension to file
